34d3c70c00d60bb9c63509cf53b4691db2b5615e
Sapan Kumar Das <sapanindia@gmail.com>
2021-01-06 22:34:20 +0530

    minor updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,8 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <center>
-                <button id="btn-nft-enable" onclick="initFirebaseMessagingRegistration()" class="btn btn-danger btn-xs btn-flat">Allow for Notification</button>
+                <button id="btn-nft-enable" onclick="initFirebaseMessagingRegistration()" 
+                class="btn btn-danger btn-xs btn-flat">Allow for Notification</button>
             </center>
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
